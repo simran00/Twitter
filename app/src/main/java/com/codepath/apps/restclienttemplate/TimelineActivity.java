d50a0d662f81e320b0b117d2b593e6039ffdf87e
Sabiha Imran <sabihaimran@fb.com>
2022-06-06 15:40:50 -0700

    logOut button fixed

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -2,8 +2,11 @@ package com.codepath.apps.restclienttemplate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
@@ -22,7 +25,10 @@ public class TimelineActivity extends AppCompatActivity {
 
         client = TwitterApp.getRestClient(this);
         populateHomeTimeline();
+        onLogoutButton();
+
     }
+
 
     private void populateHomeTimeline() {
         client.getHomeTimeline(new JsonHttpResponseHandler() {
@@ -36,5 +42,24 @@ public class TimelineActivity extends AppCompatActivity {
                 Log.e(TAG, "onFailure! " + response, throwable);
             }
         });
+    }
+
+    private void onLogoutButton() {
+        Button logOut = (Button)findViewById(R.id.logOut);
+        // forget who's logged in
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TwitterApp.getRestClient(TimelineActivity.this).clearAccessToken();
+
+                // navigate backwards to Login screen
+                Intent i = new Intent(TimelineActivity.this, LoginActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // this makes sure the Back button won't work
+                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // same as above
+                startActivity(i);
+                finish();
+            }
+        });
+
     }
 }
